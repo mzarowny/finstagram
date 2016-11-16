979149fb02b98206cd95d941029815d8ade19f6a
Mike Zarowny <mike@greenhouseinteractive.com>
2016-11-16 23:00:39 +0000

    my first method

--- app/actions.rb
+++ app/actions.rb
@@ -1,18 +1,45 @@
+def humanized_time_ago(minute_num)
+    if minute_num >= 60
+   "#{minute_num / 60} Hours ago"
+   else
+       "#{minute_num} Minutes ago"
+   end
+  end
+  
 get '/' do
     
     username = "sharky_j"
     avatar_url = "http://naserca.com/images/sharky_j.jpg"
     photo_url = "http://naserca.com/images/shark.png"
-    time_ago_in_minutes = 15 
+    time_ago_in_minutes = 30 
     like_count = 0
     comment_count = 1
     comments = [
         "sharky_j: Out for the long weekend...to emabarassed to show you the beach bod"]
-if 
-    time_ago_in_minutes > 60 
-    "More than an hour ago"
-else
-    time_ago_in_minutes < 60
-    "Less than an hour"
+        
+    humanized_time_ago (time_ago_in_minutes)
 end
-end
+        
+        
+        
+        
+        
+        
+#if 
+    #if the time_ago_in_minutes is more than 60
+  #  time_ago_in_minutes >= 60 
+   # "#{time_ago_in_minutes / 60} hours ago"
+    #return this string
+  
+    # if its less than or equal to 
+    #elsif time_ago_in_minutes == 60
+#    "An hour ago"
+ #   elsif time_ago_in_minutes <= 1
+  #  "Just a moment ago"
+        
+#else
+    #return this instead
+ #   "#{time_ago_in_minutes} minutes ago"
+   # "Less than an hour"
+#end
+#end
