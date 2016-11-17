cf1b3c02181db42d3ea743f5cac3ffee746e3915
Mike Zarowny <mike@greenhouseinteractive.com>
2016-11-17 03:35:46 +0000

    get my new hash

--- app/actions.rb
+++ app/actions.rb
@@ -1,29 +1,56 @@
-def humanized_time_ago(minute_num)
-    if minute_num >= 60
-   "#{minute_num / 60} Hours ago"
-   else
-       "#{minute_num} Minutes ago"
-   end
+def humanized_time_ago(time_ago_in_minutes)
+  if time_ago_in_minutes >= 60
+    "#{time_ago_in_minutes / 60} Hours ago"
+  else
+    "#{time_ago_in_minutes} Minutes ago"
   end
+end
   
 get '/' do
+  post_shark = {
+    username: "sharky_j",
+    avatar_url: "http://naserca.com/images/sharky_j.jpg",
+    photo_url: "http://naserca.com/images/shark.png",
+    humanized_time_ago: humanized_time_ago(15),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "sharky_j",
+      text: "Out for the long weekend...to emabarassed to show you the beach bod"
+    }]
+ }   
     
-    username = "sharky_j"
-    avatar_url = "http://naserca.com/images/sharky_j.jpg"
-    photo_url = "http://naserca.com/images/shark.png"
-    time_ago_in_minutes = 30 
-    like_count = 0
-    comment_count = 1
-    comments = [
-        "sharky_j: Out for the long weekend...to emabarassed to show you the beach bod"]
         
-    humanized_time_ago (time_ago_in_minutes)
+  post_whale = {
+    username: "kirk_whalum",
+    avatar_url: "http://naserca.com/images/kirk_whalum.jpg",
+    photo_url: "http://naserca.com/images/whale.jpg",
+    humanized_time_ago: humanized_time_ago(65),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "kirk_whalum",
+      text: "#weekendvibes"
+    }]
+}
+
+  post_marlin = {
+    username: "marlin_peppa",
+    avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+    photo_url: "http://naserca.com/images/marlin.jpg",
+    humanized_time_ago: humanized_time_ago(190),
+    like_count: 0,
+    comment_count: 1,
+    comments: [{
+      username: "marlin_peppa",
+      text: "lunchtime! ;)"
+    }]
+  }
+end        
+get '/' do
+    #....
+    [post_shark, post_whale, post_marlin].to_s
 end
-        
-        
-        
-        
-        
         
 #if 
     #if the time_ago_in_minutes is more than 60
